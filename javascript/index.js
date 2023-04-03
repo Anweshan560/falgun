d4ab075e796deb38d57f2fd7a3530c75a74a718b
AnweshanRai <anweshan2000@gmail.com>
2023-04-03 15:59:40 +0545

    variacble

--- javascript/index.js
+++ javascript/index.js
@@ -1,9 +1,95 @@
 console.log("hellow world")
 course_name="ram"
-var brand ="apple"
+
 let color="red"
-const PI =3.14
-console.log(brand,color,PI);
+let paid = true
+
+console.log(color)
+let data = null
+console.log(data)
+let brand; //declaration
+console.log(brand)
+//reference non-premitive data type:
+//object
+//array
+  
+
+//array
+
+
+let numbers=[1,2,3,4,5]
+console.log(numbers)
+let datas = ["ram" , 10,true]
+console.log(datas)
+
+
+// object
+
+let name="Ram"
+let marriage=false
+let address="lazimpat"
+
+let projector_color="white"
+let projector_price="50000"
+let projector_resolution="1900"
+
+let projector2_color="black"
+let projector2_price="10000"
+
+
+let projector1  = { 
+    color:"white",
+    price:"black",
+    resoution:"white"
+}
+
+
+
+let sita ={
+    gender:"Female",
+    skin_color:1000,
+    status:"single"
+    
+}
+
+
+
+
+let prime_numbers=[
+    1,3,5,7
+]
+
+
+
+let couses  = ["css","html",{name:"js"},{name:"react"}]
+
+// index in array
+    // always starts with 0
+
+let fruits=["apple","banana","grape"]
+console.log("second elemts of brand is ", fruits[1])
+fruits[1]="peach"
+console.log("second elemts of brand is ", fruits[1])
+console.log(fruits);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
